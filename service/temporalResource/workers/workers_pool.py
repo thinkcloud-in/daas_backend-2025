6fc3fc2215e61388eefda2e6cbc382e2ee46f2b9
KaibalyaNayak <knayak@thinkcloud.in>
2026-02-27 17:19:22 +0530

    Reapply "singletone session agaement"
    This reverts commit e717080dcce715f7809a0c7d8d2ca9ec430efcbe.

--- service/temporalResource/workers/workers_pool.py
+++ service/temporalResource/workers/workers_pool.py
@@ -1,18 +1,11 @@
-from temporalio.client import Client
 from temporalio.worker import Worker
-import os
 from service.temporalResource.activity import activities_pool
 from service.temporalResource.workflows import workflows_pool
 from fastapi import HTTPException
+from utils.temporal_client import TemporalClientManager
 
 async def connectionWithTemporal():
-    
-    try:
-        client = await Client.connect(os.getenv('TEMPORAL_SERVER'))  
-        
-        return client
-    except Exception as e:
-        raise e    
+    return await TemporalClientManager.get_temporal_client()
 
 
 async def create_pool_worker():
