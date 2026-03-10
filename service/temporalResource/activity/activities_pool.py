0e859e8861e95898f2a84c1b8120847465558e4e
hemantsthinkcloud <hemants@thinkcloud.in>
2026-03-10 17:55:30 +0530

    update cod& c:/RCV_Dev_RAQ/daas_backend-2025/.venv/Scripts/Activate.ps1 e

--- service/temporalResource/activity/activities_pool.py
+++ service/temporalResource/activity/activities_pool.py
@@ -889,35 +889,19 @@ async def domain_join_activity(pool_id: int, pool_ad_domain: str, pool_ad_passwo
             file.write(yaml_content)
             file.close()
 
-            import time
+            for vm_id in vm_ids:
 
-            cmd = f"qm set {vm_id} --cicustom user=local:snippets/join-domain-pool-{pool_id}.yml"
-
-            for i in range(3):
+                cmd = f"qm set {vm_id} --cicustom user=local:snippets/join-domain-pool-{pool_id}.yml"
 
                 stdin, stdout, stderr = ssh.exec_command(cmd)
 
                 exit_status = stdout.channel.recv_exit_status()
+                out = stdout.read().decode()
                 err = stderr.read().decode()
 
-                if "can't lock file" in err:
-                    print("VM locked, waiting...")
-                    time.sleep(5)
-                else:
-                    break
-            # for vm_id in vm_ids:
-
-            #     cmd = f"qm set {vm_id} --cicustom user=local:snippets/join-domain-pool-{pool_id}.yml"
-
-            #     stdin, stdout, stderr = ssh.exec_command(cmd)
-
-            #     exit_status = stdout.channel.recv_exit_status()
-            #     out = stdout.read().decode()
-            #     err = stderr.read().decode()
-
-            #     print("EXIT:", exit_status)
-            #     print("OUT:", out)
-            #     print("ERR:", err)
+                print("EXIT:", exit_status)
+                print("OUT:", out)
+                print("ERR:", err)
 
         except Exception as e:
             return {"status": "error", "error": f"SSH/Proxmox error: {str(e)}"}
