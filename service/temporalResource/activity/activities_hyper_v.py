58c4712ba01dc6fdbcdf5beb7758bae5ab84756d
hemantsthinkcloud <hemants@thinkcloud.in>
2026-04-13 21:12:56 +0530

    update hyper activity

--- service/temporalResource/activity/activities_hyper_v.py
+++ service/temporalResource/activity/activities_hyper_v.py
@@ -442,10 +442,10 @@ async def rebuild_machine_in_pool_activity(request: dict) -> dict:
             "os_type": template_data.get("os_type"),
             "subnet": template_data.get("subnet"),
             "dns": template_data.get("dns"),
-            "domain": template_data.get("pool_ad_domain"),
-            "ou": template_data.get("pool_ad_path"),
-            "username": template_data.get("pool_ad_username"),
-            "domain_password": template_data.get("pool_ad_password"),
+            "domain": pool.get("pool_ad_domain"),
+            "ou": pool.get("pool_ad_path"),
+            "username": pool.get("pool_ad_username"),
+            "domain_password": pool.get("pool_ad_password"),
         }
 
         async with httpx.AsyncClient(timeout=180.0) as client:
