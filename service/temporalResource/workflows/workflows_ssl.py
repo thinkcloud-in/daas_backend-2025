1d307a5d5d5c3540fe48ac299b4f4cb2ae071dec
hemantsthinkcloud <hemants@thinkcloud.in>
2026-06-09 15:51:13 +0530

    update workflows ssl

--- service/temporalResource/workflows/workflows_ssl.py
+++ service/temporalResource/workflows/workflows_ssl.py
@@ -1,9 +1,7 @@
 from temporalio import workflow
 from datetime import timedelta
-
 from temporalio.common import RetryPolicy
-with workflow.unsafe.imports_passed_through():
-    from service.temporalResource.activity import activities_ssl
+from service.temporalResource.activity import activities_ssl
 
 
 @workflow.defn
@@ -17,12 +15,15 @@ class SSLUploadWorkflow:
             maximum_attempts=5,
         )
 
-        return await workflow.execute_activity(
-            activities_ssl.upload_certificate_activity,
-            payload,
-            retry_policy=retry_policy,
-            start_to_close_timeout=timedelta(seconds=60)
-        )
+        try:
+            return await workflow.execute_activity(
+                activities_ssl.upload_certificate_activity,
+                payload,
+                retry_policy=retry_policy,
+                start_to_close_timeout=timedelta(seconds=60)
+            )
+        except Exception as e:
+            raise Exception(f"Error in SSLUploadWorkflow: {str(e)}")
 
 @workflow.defn
 class SSLRenewWorkflow:
@@ -34,12 +35,16 @@ class SSLRenewWorkflow:
             maximum_interval=timedelta(seconds=30),
             maximum_attempts=5,
         )
-        return await workflow.execute_activity(
-            activities_ssl.renew_certificate_activity,
-            payload,
-            retry_policy=retry_policy,
-            start_to_close_timeout=timedelta(seconds=60)
-        )
+
+        try:
+            return await workflow.execute_activity(
+                activities_ssl.renew_certificate_activity,
+                payload,
+                retry_policy=retry_policy,
+                start_to_close_timeout=timedelta(seconds=60)
+            )
+        except Exception as e:
+            raise Exception(f"Error in SSLRenewWorkflow: {str(e)}")
 
 @workflow.defn
 class SSLDeleteWorkflow:
@@ -51,11 +56,14 @@ class SSLDeleteWorkflow:
             maximum_interval=timedelta(seconds=30),
             maximum_attempts=5,
         )
-        return await workflow.execute_activity(
-            activities_ssl.delete_certificate_activity,
-            retry_policy=retry_policy,
-            start_to_close_timeout=timedelta(seconds=30)
-        )
+        try:
+            return await workflow.execute_activity(
+                activities_ssl.delete_certificate_activity,
+                retry_policy=retry_policy,
+                start_to_close_timeout=timedelta(seconds=30)
+            )
+        except Exception as e:
+            raise Exception(f"Error in SSLDeleteWorkflow: {str(e)}")
 
 @workflow.defn
 class SSLGetStatusWorkflow:
@@ -67,8 +75,12 @@ class SSLGetStatusWorkflow:
             maximum_interval=timedelta(seconds=30),
             maximum_attempts=5,
         )
-        return await workflow.execute_activity(
-            activities_ssl.get_certificate_status_activity,
-            retry_policy=retry_policy,
-            start_to_close_timeout=timedelta(seconds=30)
-        )
+
+        try:
+            return await workflow.execute_activity(
+                activities_ssl.get_certificate_status_activity,
+                retry_policy=retry_policy,
+                start_to_close_timeout=timedelta(seconds=30)
+            )
+        except Exception as e:
+            raise Exception(f"Error in SSLGetStatusWorkflow: {str(e)}")
